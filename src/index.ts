66d9b1a306b7b87077a0908ce2c3845b268a9560
Akshay.s <a.sutar@saniconservices.com>
2025-04-01 12:15:40 +0530

    removed env config

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import path from "path";
 import { startServer } from "./app/app";
 // Load environment variables from .env file
+dotenv.config();
 
 const PORT = process.env.App_Port || 3034;
 startServer();
